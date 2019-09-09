6881483bcd78019181ac5e82e00c84ba04431eea
andres-abrigo <55062266+andres-abrigo@users.noreply.github.com>
2019-09-09 12:53:25 -0300

    Update database_connection.py

--- database_connection.py
+++ database_connection.py
@@ -4,12 +4,12 @@ import pandas_functions
 
 user = 'username'
 password = 'password'
-port = 3303
+port = port_number
 
 IP = '127.0.0.1'
 database_name = 'database_name'
 
-with pymysql.connect(host= IP, port=port, user=user, passwd=password, db=database_name) as connection:
+with pymysql.connect(host= IP, port=port_number, user=user, passwd=password, db=database_name) as connection:
   query_table = """SELECT * FROM table"""
   dataframe_table = pandas_functions.dataframe_from_query(query, connection)
   print(dataframe_table.to_string())
