1f77bafb1669850350ef910b3fac2d6277cd8bce
andres-abrigo <55062266+andres-abrigo@users.noreply.github.com>
2019-09-12 09:12:29 -0300

    Update database_connection.py

--- database_connection.py
+++ database_connection.py
@@ -9,13 +9,17 @@ port = port_number
 IP = '127.0.0.1'
 database_name = 'database_name'
 
-with pymysql.connect(host= IP, port=port_number, user=user, passwd=password, db=database_name) as connection:
-  query_table = """SELECT * FROM table"""
-  dataframe_table = pandas_functions.dataframe_from_query(query, connection)
-  print(dataframe_table.to_string())
-  
-  output_excel_path = 'result.xlsx'
-  sheet_name = 'sql_table'
+connection = pymysql.connect(host= IP, port=port, user=user, passwd=password, db=database_name)
 
-  pandas_functions.save_dataframe_to_excel(dataframe_table,output_excel_path, sheet_name)
-                           
+query_table = """SELECT * FROM ViewDebugInfo2"""
+
+dataframe_table = pandas_functions.read_sql_query_to_dataframe(query_table, connection)
+
+print(dataframe_table.to_string())
+
+output_excel_path = 'result.xlsx'
+sheet_name = 'sql_table'
+
+pandas_functions.save_dataframe_to_excel(dataframe_table,output_excel_path, sheet_name)
+
+connection.close()
