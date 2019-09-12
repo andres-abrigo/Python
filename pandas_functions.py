e3c7423016f4a504a84db8ad0d219a9a8b1844a4
andres-abrigo <55062266+andres-abrigo@users.noreply.github.com>
2019-09-12 09:13:03 -0300

    Update pandas_functions.py

--- pandas_functions.py
+++ pandas_functions.py
@@ -3,3 +3,9 @@ import pandas as pd
 def read_sql_query_to_dataframe(query, connection):
     df = pd.read_sql(query,con=connection)
     return df
+
+def save_dataframe_to_excel(dataframe, excel_file_path, sheet_name):
+    writer = pd.ExcelWriter(excel_file_path)
+    dataframe.to_excel(writer,sheet_name)
+    writer.save()
+    
